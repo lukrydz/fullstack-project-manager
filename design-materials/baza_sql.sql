7fbad114d3b7ce5aca78000e385a6a98ff095add
Slawomir-J <nonseyy@gmail.com>
2021-04-14 21:16:58 +0200

    Update baza_sql.sql

--- design-materials/baza_sql.sql
+++ design-materials/baza_sql.sql
@@ -11,12 +11,29 @@ TABLESPACE pg_default;
 ALTER TABLE public.users
     OWNER to postgres;
 
+CREATE TABLE public.sessions
+(
+    session_id integer NOT NULL,
+    user_id integer NOT NULL,
+    expiration_date date NOT NULL,
+    CONSTRAINT sessions_pkey PRIMARY KEY (session_id),
+    CONSTRAINT sessions_user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES public.users (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.sessions
+    OWNER to postgres;
+
 CREATE TABLE public.boards
 (
     board_id integer NOT NULL,
-    title character varying(20) COLLATE pg_catalog."default",
-    status boolean,
+    archived boolean,
     user_id integer NOT NULL,
+    name character varying(20)[] COLLATE pg_catalog."default",
     CONSTRAINT boards_pkey PRIMARY KEY (board_id),
     CONSTRAINT boards_user_id_fkey FOREIGN KEY (user_id)
         REFERENCES public.users (user_id) MATCH SIMPLE
@@ -28,6 +45,7 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.boards
     OWNER to postgres;
+
 
 CREATE TABLE public.columns
 (
@@ -46,13 +64,15 @@ TABLESPACE pg_default;
 ALTER TABLE public.columns
     OWNER to postgres;
 
+
+
 CREATE TABLE public.cards
 (
     card_id integer NOT NULL,
     name character varying(20) COLLATE pg_catalog."default" NOT NULL,
     column_id integer NOT NULL,
-    status boolean NOT NULL,
-    "order" int NOT NULL,
+    archived boolean NOT NULL,
+    "order" numeric NOT NULL,
     CONSTRAINT cards_pkey PRIMARY KEY (card_id),
     CONSTRAINT cards_column_id_fkey FOREIGN KEY (column_id)
         REFERENCES public.columns (column_id) MATCH SIMPLE
@@ -63,13 +83,12 @@ CREATE TABLE public.cards
 TABLESPACE pg_default;
 
 ALTER TABLE public.cards
-    OWNER to postgres;
+    OWNER to postgres;    
 
 CREATE TABLE public.public_boards
 (
     public_boards_id integer NOT NULL,
-    title character varying(20) COLLATE pg_catalog."default",
-    status boolean NOT NULL,
+    name character varying(20)[] COLLATE pg_catalog."default",
     CONSTRAINT public_boards_pkey PRIMARY KEY (public_boards_id)
 )
 
@@ -77,6 +96,7 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.public_boards
     OWNER to postgres;
+
 
 CREATE TABLE public.public_columns
 (
@@ -100,8 +120,7 @@ CREATE TABLE public.public_cards
     public_cards_id integer NOT NULL,
     name character varying(50) COLLATE pg_catalog."default",
     public_column_id integer NOT NULL,
-    status boolean,
-    "order" int NOT NULL,
+    "order" numeric,
     CONSTRAINT public_cards_pkey PRIMARY KEY (public_cards_id),
     CONSTRAINT public_cards_public_column_id_fkey FOREIGN KEY (public_column_id)
         REFERENCES public.public_columns (public_column_id) MATCH SIMPLE
@@ -112,4 +131,4 @@ CREATE TABLE public.public_cards
 TABLESPACE pg_default;
 
 ALTER TABLE public.public_cards
-    OWNER to postgres;
+    OWNER to postgres;    
